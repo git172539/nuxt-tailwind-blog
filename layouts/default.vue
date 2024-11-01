15024cbec825c87670da711e9c969c52ea7ad02b
peem_shadow <rattapak.peem@gmail.com>
2024-11-02 04:38:59 +0700

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <!-- HEADER -->
+    <AppHeader />
+
+    <div class="container px-4 py-4 mx-auto max-w-7xl lg:px-8 sm:px-0">
+      <slot />
+    </div>
+
+    <AppFooter />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const { data: posts, error } = await useWPAPI().getPosts();
+console.log(posts, error);
+</script>
+<template>
+  <main>
+    <!-- Blog Section Starts -->
+    <section class="container mx-auto">
+      <div class="container py-10">
+        <div class="grid gap-10 sm:grid-cols-3">
+          <BlogGrid
+            v-for="post in posts"
+            :key="post.id"
+            :title="post.title.rendered"
+            :image="post._embedded['wp:featuredmedia'][0]?.source_url"
+            :excerpt="post.excerpt.rendered"
+            :slug="post.slug"
+          >
+          </BlogGrid>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<style scoped></style>
